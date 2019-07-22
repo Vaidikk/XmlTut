e4061076962ccaec7fe9771f82c6a5bf4bd344f6
vaidik <vedik.khandelwal@gmail.com>
2019-07-22 18:06:34 +0530

    Updated class MovieXmsHandler

---
+++ XmlXsdTest/src/com/riptide/xml/sax/parsers/MovieXmlHandler.java
@@ -0,0 +1,41 @@
+package com.riptide.xml.sax.parsers;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class MovieXmlHandler extends DefaultHandler {
+
+	@Override
+	public void startDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		System.out.println("Starting...");
+	}
+
+	@Override
+	public void endDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		System.out.println("End of doc...");
+	}
+
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		// TODO Auto-generated method stub
+		System.out.println("Start of Element: "+qName);
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		// TODO Auto-generated method stub
+		System.out.println("End of Element: "+qName);
+	}
+
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		// TODO Auto-generated method stub
+		for(int i=start; i<start+length; i++)
+			System.out.print(ch[i]);
+		System.out.println();
+	}
+
+}
